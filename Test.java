365bf7500ca656e26739bb265e4b367b6dd004d6
Brian Chukwuisiocha <81457860+BrianChiedu@users.noreply.github.com>
2023-11-02 15:26:49 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,34 +1,42 @@
-package dev.brymocodes;
+adj_list[tree_to[i]].append(tree_from[i])
 
-public class Test {
-    public static void main(String[] args) {
-        //System.out.println(solver(new String[]{"1","2","3","4","5","6","7"}));
-        String[] expression = {"1", "1", "1", "o", "Twenty Two", " 1  ", "4"};
-        System.out.println(solver(expression));
-    }
+    # Helper function to calculate the distance between two nodes using BFS
+    def calculate_distance(start, end):
+        visited = [False] * tree_nodes
+        distance = [0] * tree_nodes
+        queue = [start]
+        visited[start] = True
 
-    public static double solver(String[] expression){
+        while queue:
+            current_node = queue.pop(0)
+            for neighbor in adj_list[current_node]:
+                if not visited[neighbor]:
+                    visited[neighbor] = True
+                    distance[neighbor] = distance[current_node] + 1
+                    queue.append(neighbor)
 
-        double[] input = new double[expression.length];
-        for(int i = 0; i < expression.length; i++){
-            try {
-                input[i] = Double.parseDouble(expression[i]);
-            } catch (NumberFormatException nfe) {
-                input[i] = 0;;
-            }
-        }
+                    if neighbor == end:
+                        return distance[neighbor]
 
-        return add(input.length-1, input);
-    }
+        # If there's no path between start and end, return None
+        return None
 
+    # Check if a Pythagorean triple is formed
+    def is_pythagorean_triple(a, b, c):
+        if a is not None and b is not None and c is not None:
+            return a + b == c or a + c == b or b + c == a
+        return False
 
-    public static double add(int n, double... a){
-        if (n == 0) {
-            return a[n];
-        }
-        else {
-            return a[n] + add(n - 1, a);
-        }
-    }
+    count = 0
+    # Iterate through all possible triples (x, y, z)
+    for a in range(tree_nodes):
+        for b in range(a + 1, tree_nodes):
+            for c in range(b + 1, tree_nodes):
+                distance_x = calculate_distance(a, x)
+                distance_y = calculate_distance(b, y)
+                distance_z = calculate_distance(c, z)
 
-}
+                if is_pythagorean_triple(distance_x, distance_y, distance_z):
+                    count += 1
+
+    return count
